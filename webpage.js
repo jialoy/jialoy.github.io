b20da137b154b5b76c0e6ea7a35faddaba6c2b98
Jia <jia@macbook-pro.home>
2018-08-10 11:30:32 +0100

    webpage build in progress

--- webpage.js
+++ webpage.js
@@ -1,21 +1,24 @@
 var about = document.getElementById("about");
 var research = document.getElementById("research");
+var cv = document.getElementById("cv");
 var aboutLink = document.getElementById("aboutLink");
 var researchLink = document.getElementById("researchLink");
 var cvLink = document.getElementById("cvLink");
 
 aboutLink.addEventListener("click", function() {
 	research.style.display = "none";
+	cv.style.display = "none";
 	about.style.display = "block";
 });
 
 researchLink.addEventListener("click", function() {
 	about.style.display = "none";
+	cv.style.display = "none";
 	research.style.display = "block";
 })
 
 cvLink.addEventListener("click", function() {
 	about.style.display = "none";
 	research.style.display = "none";
-	alert("cv download to be added once updated");
+	cv.style.display = "block";
 })
