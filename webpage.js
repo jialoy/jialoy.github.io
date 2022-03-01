6ee524f589b664ff5412cdc3ad73e851c18d33b8
jialoy <jiaern.loy@gmail.com>
2022-03-01 14:12:58 +0100

    update because github...

---
+++ webpage.js
@@ -0,0 +1,36 @@
+var about = document.getElementById("about");
+var research = document.getElementById("research");
+var teaching = document.getElementById("teaching");
+var cv = document.getElementById("cv");
+var aboutLink = document.getElementById("aboutLink");
+var researchLink = document.getElementById("researchLink");
+var teachingLink = document.getElementById("teachingLink");
+var cvLink = document.getElementById("cvLink");
+
+aboutLink.addEventListener("click", function() {
+	research.style.display = "none";
+	teaching.style.display = "none";
+	cv.style.display = "none";
+	about.style.display = "block";
+});
+
+researchLink.addEventListener("click", function() {
+	about.style.display = "none";
+	teaching.style.display = "none";
+	cv.style.display = "none";
+	research.style.display = "block";
+});
+
+teachingLink.addEventListener("click", function() {
+	about.style.display = "none";
+	research.style.display = "none";
+	cv.style.display = "none";
+	teaching.style.display = "block";
+});
+
+cvLink.addEventListener("click", function() {
+	about.style.display = "none";
+	research.style.display = "none";
+	teaching.style.display = "none";
+	cv.style.display = "block";
+});
