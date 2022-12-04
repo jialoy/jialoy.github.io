034e618bf34fba791c48436006d726bcae1d2c36
jialoy <jiaern.loy@gmail.com>
2022-12-05 00:45:31 +0100

    update side things

--- webpage.js
+++ webpage.js
@@ -1,10 +1,12 @@
 var about = document.getElementById("about");
 var research = document.getElementById("research");
 var teaching = document.getElementById("teaching");
+var sidethings = document.getElementById("sidethings");
 var cv = document.getElementById("cv");
 var aboutLink = document.getElementById("aboutLink");
 var researchLink = document.getElementById("researchLink");
 var teachingLink = document.getElementById("teachingLink");
+var sidethingsLink = document.getElementById("sidethingsLink");
 var cvLink = document.getElementById("cvLink");
 
 aboutLink.addEventListener("click", function() {
@@ -12,6 +14,7 @@ aboutLink.addEventListener("click", function() {
 	teaching.style.display = "none";
 	cv.style.display = "none";
 	about.style.display = "block";
+	sidethings.style.display = "none";
 });
 
 researchLink.addEventListener("click", function() {
@@ -19,6 +22,7 @@ researchLink.addEventListener("click", function() {
 	teaching.style.display = "none";
 	cv.style.display = "none";
 	research.style.display = "block";
+	sidethings.style.display = "none";
 });
 
 teachingLink.addEventListener("click", function() {
@@ -26,6 +30,15 @@ teachingLink.addEventListener("click", function() {
 	research.style.display = "none";
 	cv.style.display = "none";
 	teaching.style.display = "block";
+	sidethings.style.display = "none";
+});
+
+sidethingsLink.addEventListener("click", function () {
+	about.style.display = "none";
+	research.style.display = "none";
+	cv.style.display = "none";
+	teaching.style.display = "none";
+	sidethings.style.display = "block";
 });
 
 cvLink.addEventListener("click", function() {
@@ -33,4 +46,5 @@ cvLink.addEventListener("click", function() {
 	research.style.display = "none";
 	teaching.style.display = "none";
 	cv.style.display = "block";
+	sidethings.style.display = "none";
 });
