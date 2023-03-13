f208119ae7ca875b316780b9a61f0dfc45ee26c7
jialoy <jiaern.loy@gmail.com>
2023-03-13 22:07:47 +0100

    update

--- webpage.js
+++ webpage.js
@@ -2,17 +2,17 @@ var about = document.getElementById("about");
 var research = document.getElementById("research");
 var teaching = document.getElementById("teaching");
 var sidethings = document.getElementById("sidethings");
-var cv = document.getElementById("cv");
+//var cv = document.getElementById("cv");
 var aboutLink = document.getElementById("aboutLink");
 var researchLink = document.getElementById("researchLink");
 var teachingLink = document.getElementById("teachingLink");
 var sidethingsLink = document.getElementById("sidethingsLink");
-var cvLink = document.getElementById("cvLink");
+//var cvLink = document.getElementById("cvLink");
 
 aboutLink.addEventListener("click", function() {
 	research.style.display = "none";
 	teaching.style.display = "none";
-	cv.style.display = "none";
+	//cv.style.display = "none";
 	about.style.display = "block";
 	sidethings.style.display = "none";
 });
@@ -20,7 +20,7 @@ aboutLink.addEventListener("click", function() {
 researchLink.addEventListener("click", function() {
 	about.style.display = "none";
 	teaching.style.display = "none";
-	cv.style.display = "none";
+	//cv.style.display = "none";
 	research.style.display = "block";
 	sidethings.style.display = "none";
 });
@@ -28,7 +28,7 @@ researchLink.addEventListener("click", function() {
 teachingLink.addEventListener("click", function() {
 	about.style.display = "none";
 	research.style.display = "none";
-	cv.style.display = "none";
+	//cv.style.display = "none";
 	teaching.style.display = "block";
 	sidethings.style.display = "none";
 });
@@ -36,7 +36,7 @@ teachingLink.addEventListener("click", function() {
 sidethingsLink.addEventListener("click", function () {
 	about.style.display = "none";
 	research.style.display = "none";
-	cv.style.display = "none";
+	//cv.style.display = "none";
 	teaching.style.display = "none";
 	sidethings.style.display = "block";
 });
@@ -45,6 +45,6 @@ cvLink.addEventListener("click", function() {
 	about.style.display = "none";
 	research.style.display = "none";
 	teaching.style.display = "none";
-	cv.style.display = "block";
+	//cv.style.display = "block";
 	sidethings.style.display = "none";
 });
